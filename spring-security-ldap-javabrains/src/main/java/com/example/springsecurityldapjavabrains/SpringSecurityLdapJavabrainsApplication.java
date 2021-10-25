29ec518f3931f0a06569c143b52013e543faf869
Methma Peiris <methma34@gmail.com>
2021-10-25 21:32:49 +0530

    Initial commit, Add all the default files

---
+++ spring-security-ldap-javabrains/src/main/java/com/example/springsecurityldapjavabrains/SpringSecurityLdapJavabrainsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityldapjavabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdapJavabrainsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLdapJavabrainsApplication.class, args);
+    }
+
+}
---
+++ spring-security-ldap-javabrains/src/test/java/com/example/springsecurityldapjavabrains/SpringSecurityLdapJavabrainsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityldapjavabrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapJavabrainsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
