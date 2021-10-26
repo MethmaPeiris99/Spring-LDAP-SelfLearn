b6c91b0adff24b826c3971f605c472a77c590087
Methma Peiris <methma34@gmail.com>
2021-10-26 11:10:49 +0530

    Initial commit, Add all the default files

---
+++ spring-security-ldap-doc/src/main/java/com/example/springsecurityldapdoc/SpringSecurityLdapDocApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityldapdoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdapDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLdapDocApplication.class, args);
+    }
+
+}
---
+++ spring-security-ldap-doc/src/test/java/com/example/springsecurityldapdoc/SpringSecurityLdapDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityldapdoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
